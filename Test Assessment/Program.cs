810cf26cd837996788ae4419bd48f55b37995b9f
Shewchuk06 <Shewchuk06@gmail.com>
2025-03-29 09:51:22 +0100

    Add project files.

---
+++ Test Assessment/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Test_Assessment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class 
+}

16111e1540750f5b51569c78766fd88585de7bbd
Shewchuk06 <Shewchuk06@gmail.com>
2025-03-29 13:19:08 +0100

    add indexes, fix model

--- DbModel/TaxiTrip.cs
+++ DbModel/TaxiTrip.cs
@@ -1,27 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Test_Assessment.DbModel
 {
+    [Index(nameof(TripDistance))]
+    [Index(nameof(PULocationID))]
+    [Index(nameof(TpepDropoffDatetime), nameof(TpepPickupDatetime))]
     public class TaxiTrip : BaseEntity
     {
-        public DateTime TpepPickupDatetime { get; set; }
+        public DateTime? TpepPickupDatetime { get; set; }
 
-        public DateTime TpepDropoffDatetime { get; set; }
+        public DateTime? TpepDropoffDatetime { get; set; }
 
-        public int PassengerCount { get; set; }
+        public int? PassengerCount { get; set; }
 
-        public double TripDistance { get; set; }
+        public double? TripDistance { get; set; }
 
-        [Required]
         [MaxLength(3)]
-        public string StoreAndFwdFlag { get; set; } = default!;
+        public string? StoreAndFwdFlag { get; set; } = default!;
 
-        public int PULocationID { get; set; }
+        public int? PULocationID { get; set; }
 
-        public int DOLocationID { get; set; }
+        public int? DOLocationID { get; set; }
 
-        public decimal FareAmount { get; set; }
+        public decimal? FareAmount { get; set; }
 
-        public decimal TipAmount { get; set; }
+        public decimal? TipAmount { get; set; }
     }
 }

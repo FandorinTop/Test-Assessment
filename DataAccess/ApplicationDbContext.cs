7a1a3bfb2b402cea080a9808648256b27d3f4c46
Shewchuk06 <Shewchuk06@gmail.com>
2025-03-29 14:13:15 +0100

    add entity framework

---
+++ DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test_Assessment.DbModel;
+
+namespace Test_Assessment.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<TaxiTrip> TaxiTrips { get; set; }
+
+        public ApplicationDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TaxiTrips;Trusted_Connection=True;");
+        }
+    }
+}
